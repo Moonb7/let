5f64451e9f55b2d22945cc46770cb8b482f2ab53
Jinsu_Mun <answlstn13@gmail.com>
2024-08-26 23:45:17 +0900

    링크드 리스트 구현해보기

--- JavaScript-Study/알고리즘_도전반_문제/zoom알고리즘연습.js
+++ JavaScript-Study/알고리즘_도전반_문제/zoom알고리즘연습.js
@@ -25,19 +25,17 @@ class Node {
 class LinkedList {
     constructor(value) {
         this.head = new Node(value);
-        this.nodeCount = 1;
     }
 
-    apend(value) {
+    apend(value) { // Tail쪽까지 추적해서 추가
         let current = this.head;
         while(current.next !== null){
             current = current.next;
         }
         current.next = new Node(value);
-        this.nodeCount++;
     }
 
-    getNode(index){
+    getNode(index){ // 마치 배열의 인덱스처럼 똑같은 원리인것 같다. index만큼 추척하여 해당 순서의 노드를 찾을 수 있다.
         let node = this.head;
         for (let i = 0; i < index; i++) {
             node = node.next;
@@ -45,10 +43,11 @@ class LinkedList {
         return node;
     }
 
-    addNode(index, value) {
-        const newNode = new Node(value);      // 새로만들고
-        let getNode = this.getNode(index-1);  // 추가할 자리
+    addNode(index, value) { // 중간에 새로운 노드를 추가할때 사용하는 함수
+        const newNode = new Node(value);      // 추가할 노드 새로만들고
+        let getNode = this.getNode(index-1);  // 추가할 자리를 확인
+        const nextNode = getNode.next;        // 뒤로 밀릴 노드를 임시로 저장
         getNode.next = newNode;               // 추가
-        newNode.next = this.getNode(index);   // 추가한 노드에 원래 있어야될 node다시 연결
+        newNode.next = nextNode;              // 원래 있어야될 노드다시 연결
     }
 }
